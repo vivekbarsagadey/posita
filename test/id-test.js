a96fd33189e4a82d765f7302ac86748234cc094b
Vivek Barsagadey <Vivek Barsagadey@CelerantCanadaWRMS>
2015-09-28 18:44:48 +0530

    Added Javascript test cases

---
+++ test/id-test.js
@@ -0,0 +1,29 @@
+
+
+var p = NAMESPACE.resource(101);
+var q = NAMESPACE.resource(102);
+var r = NAMESPACE.resource(103);
+
+// Checking for Resource Ids.
+QUnit.test( "Checking for Resource Ids", function( assert ) {
+    assert.ok(p.getId() == 101, "Result : P Passed!" );
+    assert.ok(q.getId() == 102, "Result : Q Passed!" );
+    assert.ok(r.getId() == 103, "Result : R Passed!" );
+});
+
+// Accessing Expensive resources.
+
+QUnit.test( "Accessing Expensive resources", function( assert ) {
+    assert.ok(p.getExpensiveResource().value == "I'm a very expensive resource associated with ID 101", "Result : P Passed!" );
+    assert.ok(q.getExpensiveResource().value == "I'm a very expensive resource associated with ID 102", "Result : q Passed!" );
+    assert.ok(r.getExpensiveResource().value == "I'm a very expensive resource associated with ID 103", "Result : r Passed!" );
+
+});
+
+// After Close operations
+
+QUnit.test( "Checking for Resource Ids", function( assert ) {
+    p.close();
+    assert.ok(p.getExpensiveResource().value =="Resource associated with ID 101 is already closed", "Result : P Passed!" );
+    assert.ok(q.getExpensiveResource().value =="I'm a very expensive resource associated with ID 102", "Result : q Passed!");
+});
